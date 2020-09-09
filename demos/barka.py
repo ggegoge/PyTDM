bb3aca2e36b32a009727391e1dd60fc53dcc7642
test0wanie <33847650+TichyProgs@users.noreply.github.com>
2020-09-09 13:42:08 +0200

    Add files via upload

---
+++ demos/barka.py
@@ -0,0 +1,50 @@
+from mowa import *
+
+hymn = """Pan kiedyś stanął nad brzegiem, 
+Szukał ludzi gotowych pójść za Nim; 
+By łowić serca 
+Słów Bożych prawdą.
+
+ 
+O Panie, to Ty na mnie spojrzałeś, 
+Twoje usta dziś wyrzekły me imię. 
+Swoją barkę pozostawiam na brzegu, 
+Razem z Tobą nowy zacznę dziś łów. 
+
+
+Jestem ubogim człowiekiem, 
+Moim skarbem są ręce gotowe 
+Do pracy z Tobą 
+I czyste serce. 
+
+
+O Panie, to Ty na mnie spojrzałeś, 
+Twoje usta dziś wyrzekły me imię. 
+Swoją barkę pozostawiam na brzegu, 
+Razem z Tobą nowy zacznę dziś łów. 
+
+
+Ty, potrzebujesz mych dłoni, 
+Mego serca młodego zapałem 
+Mych kropli potu 
+I samotności. 
+
+
+O Panie, to Ty na mnie spojrzałeś, 
+Twoje usta dziś wyrzekły me imię. 
+Swoją barkę pozostawiam na brzegu, 
+Razem z Tobą nowy zacznę dziś łów. 
+
+
+Dziś wypłyniemy już razem 
+Łowić serca na morzach dusz ludzkich 
+Twej prawdy siecią 
+I słowem życia. 
+"""
+
+zwrotki = hymn.split('\n\n')
+
+mów('Zaśpiewam ulubioną pieśń ojca świętego Jana Pawła drugiego!\n')
+for zwrotka in zwrotki:
+	mów(zwrotka)
+	print('\n\n')
