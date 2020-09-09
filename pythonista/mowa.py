8485246b9ac9f384ded641da9821f1ba88b8a9d2
test0wanie <33847650+TichyProgs@users.noreply.github.com>
2020-09-09 13:56:27 +0200

    Create mowa.py

---
+++ pythonista/mowa.py
@@ -0,0 +1,24 @@
+'''PyTDM's version for Pythonista'''
+
+import speech
+# relative vs non relative import bs
+try:
+	from translacja import tłumacz
+except ModuleNotFoundError:
+	from .translacja import tłumacz
+
+
+
+# engine = pyttsx3.init()
+
+def say(s):
+	speech.say(s)
+	# engine.runAndWait()
+
+
+def mów(s):
+	# apple's text to speech doesn't handle all those signs apart from commas too well
+	s_corrected = s.replace(".", ",").replace("!", ",").replace(":", ",").replace("?", ",")
+	translated = tłumacz(s.lower())
+	print(s)
+	say(translated)
