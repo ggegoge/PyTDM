886399750f5423dd135547721431bb4ba9f8f6d0
ggegoge <33847650+test0wanie@users.noreply.github.com>
2020-09-10 13:44:00 +0200

    v num changed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as f:
 setup(
 	name='pytdm',
 	url='https://github.com/test0wanie/PyTDM',
-	version='0.0.6',
+	version='0.0.7',
 	description='Pytońska treść do mowy – Polish text to speech',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
