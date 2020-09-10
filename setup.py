655d172f2b4031b8afedfbb8fb3a9e0b343fec2d
ggegoge <33847650+test0wanie@users.noreply.github.com>
2020-09-10 11:39:42 +0200

    tabs corrected (I hope it indeed is 4)

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
-from setuptools import setup 
+from setuptools import setup #, find_packages,
 
 with open("README.md", 'r') as f:
 	long_description = f.read()
 	
 setup(
-    name='pytdm',
+	name='pytdm',
 	url='https://github.com/test0wanie/PyTDM',
 	version='0.0.4',
-    description='Pytońska treść do mowy – Polish text to speech',
+	description='Pytońska treść do mowy – Polish text to speech',
 	long_description=long_description,
-    long_description_content_type="text/markdown",
-    author='test0wanie',
+	long_description_content_type="text/markdown",
+	author='test0wanie',
 	author_email='test0wanie@protonmail.com',
-    packages=['pytdm'],
+	packages=['pytdm'],
 	install_requires=['pyttsx3'],
-    license='MIT',
+	license='MIT',
 	classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
 		"Development Status :: 2 - Pre-Alpha",
-    ],
-    python_requires='>=3.6',
+	],
+	python_requires='>=3.6',
 )
