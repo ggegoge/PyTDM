4dc835694c0d49525e0d7fbb8e0df92686c479d2
ggegoge <33847650+test0wanie@users.noreply.github.com>
2020-09-10 13:42:06 +0200

    0.0.7 - alternative func names

--- pytdm/__init__.py
+++ pytdm/__init__.py
@@ -1,2 +1,10 @@
 from .mowa import mów, tłumacz
 from .translacja import repolonizuj, anglicyzuj
+
+# to make it no-polish-keyboard friendly:
+
+def mow(s, show=True):
+	mów(s, show)
+
+def tlumacz(s):
+	return tłumacz(s)
